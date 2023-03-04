742c735ac0064991116dde3d9569b19f6f5fefca
Joshua Riefman <joshuariefman@gmail.com>
2023-03-03 20:01:30 -0800

    Missed a file i

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -8,7 +8,7 @@ public class TimeManager : MonoBehaviour
 {
     [SerializeField] private Volume dayVolume;
     [SerializeField] private Volume nightVolume;
-    [SerializeField] private Volume realTimeVolume;
+    [SerializeField] private LightingSettings lightingSettings;
 
     [System.Serializable]
     public enum TimeState
@@ -22,20 +22,15 @@ public class TimeManager : MonoBehaviour
     public TimeState timeState;
 
     [SerializeField] private float time;
-    [SerializeField] private float timeStateCounter = 300;
+    [SerializeField] private float timeStateCounter = 5;
 
     public Dictionary<TimeState, float> timeStateLengths = new Dictionary<TimeState, float>()
     {
-        [TimeState.Day] = 300,
-        [TimeState.Twilight] = 300,
-        [TimeState.Night] = 300,
-        [TimeState.Morning] = 300
+        [TimeState.Day] = 5,
+        [TimeState.Twilight] = 5,
+        [TimeState.Night] = 5,
+        [TimeState.Morning] = 5
     };
-
-    private void Start()
-    {
-        throw new NotImplementedException();
-    }
 
     private void Update()
     {
@@ -46,11 +41,42 @@ public class TimeManager : MonoBehaviour
         {
             AdvanceTimeState();
         }
+
+        UpdatePostProcessing();
+        UpdateLighting();
     }
 
     void AdvanceTimeState()
     {
         timeState = (int)timeState > 2 ? TimeState.Day : timeState + 1;
         timeStateCounter = timeStateLengths[timeState];
+    }
+
+    void UpdatePostProcessing()
+    {
+        switch (timeState)
+        {
+            case TimeState.Day:
+                dayVolume.weight = 1;
+                nightVolume.weight = 0;
+                break;
+            case TimeState.Twilight:
+                nightVolume.weight = 1 - (timeStateCounter / timeStateLengths[TimeState.Twilight]);
+                dayVolume.weight = timeStateCounter / timeStateLengths[TimeState.Twilight];
+                break;
+            case TimeState.Night:
+                nightVolume.weight = 1;
+                dayVolume.weight = 0;
+                break;
+            case TimeState.Morning:
+                dayVolume.weight = 1 - (timeStateCounter / timeStateLengths[TimeState.Morning]);
+                nightVolume.weight = timeStateCounter / timeStateLengths[TimeState.Morning];
+                break;
+        }
+    }
+
+    void UpdateLighting()
+    {
+        
     }
 }
