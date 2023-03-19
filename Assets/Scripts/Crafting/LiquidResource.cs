eb8db11739e69ed12ccd60fe697526ab2eb05494
Kyle Robert van Winkoop <kylevanwinkoop@gmail.com>
2023-03-19 16:10:25 -0700

    Messed with the scriptables
    Organized the scriptables into folders
    
    Also made a new scriptable "ResourceType" that represents a type of "liquid resource" for the new crafting

---
+++ Assets/Scripts/Crafting/LiquidResource.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//A instance of the type
+[System.Serializable]
+public class LiquidResource
+{
+    public ResourceType resource;
+    public float amount;
+}
--- Assets/Scripts/Crafting/Recipe.cs
+++ Assets/Scripts/Crafting/Recipe.cs
@@ -8,14 +8,24 @@ using UnityEngine;
 public class Recipe : ScriptableObject
 {
     [Serializable]
-    public struct Pair
+    public struct Pair1
     {
         public int amount;
         public Item item;
     }
     
+    [Serializable]
+    public struct Pair2
+    {
+        public int amount;
+        public ResourceType res;
+    }
+    
     [SerializeField]
-    public List<Pair> needed;
+    public List<Pair1> needed;
+
+    [SerializeField]
+    public List<Pair2> liquids;
     //Could be replaced with the serialized pair.
     //In case more then 1 of the resulting item is made.
     public Item created;
